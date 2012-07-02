4d035c8b9950ac3229be4d860590e37fdaac920b
Justin Raczak <jraczak@gmail.com>
2012-07-02 16:12:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sonar::Application.config.secret_token = '4148e40cb84a4a623607f40418efb07c032139a61677925b2d56b0118d8a1c93dfec2b059ccf932e73e142aecd93694534f78c76df4ee971a8c4044e149c092b'
