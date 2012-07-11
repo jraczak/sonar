788666bd8aa9ba611835ed259a98be65ced4cfd0
Justin Raczak <jraczak@gmail.com>
2012-07-11 18:00:45 -0400

    Fill in header, footer, add global CSS/Bootstrap

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,56 +2,38 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  subject { page }
+
   describe "Home page" do
   
-    it "should have the content 'Sonar'" do
-      visit "/static_pages/home"
-      page.should have_selector("h1", :text => "Sonar")
-    end
-      
-    it "should have the base title" do
-      visit "/static_pages/home"  
-      page.should have_selector("title", :text => "Sonar")
-    end
+    before { visit root_path }
     
-    it "should not have a custom page title" do
-      visit "/static_pages/home"
-      page.should_not have_selector("title", :text => "| Welcome!")
-    end
+    it { should have_selector("h1", :text => "Sonar") }
+    it { should have_selector("title", :text => full_title("")) }
+    it { should_not have_selector("title", :text => "| Welcome!") }
   end
   
   describe "Help page" do
   
-    it "should have the content 'Help'" do
-      visit "/static_pages/help"
-      page.should have_selector("h1", :text => "Help")
-    end
+    before { visit help_path }
     
-    it "should have the full title" do
-      visit "/static_pages/help"
-      page.should have_selector("title", :text => "Sonar | Help")
-    end
+    it { should have_selector("h1", :text => "Help") }
+    it { should have_selector("title", :text => full_title("Help")) }
   end
   
   describe "About page" do
   
-    it "should have the content 'About Sonar'" do
-      visit "/static_pages/about"
-      page.should have_selector("h1", :text => "About Sonar")
-    end
+    before { visit about_path }
     
-    it "should have the full title" do
-      visit "/static_pages/about"
-      page.should have_selector("title", :text => "Sonar | About")
-    end
+    it { should have_selector("h1", :text => "About Sonar")}
+    it { should have_selector("title", :text => full_title("About")) }
   end
   
   describe "Contact page" do
   
-    it "should have the content 'Contact'" do
-      visit "/static_pages/contact"
-      page.should have_selector("title", :text => "Sonar | Contact")
-    end
+    before { visit contact_path }
+    
+    it { should have_selector("title", :text => full_title("Contact")) }
   end
   
 end
